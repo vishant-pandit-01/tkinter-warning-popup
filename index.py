7c153c89c36b088181014e0c74e3c2f4684ae999
VISHANT PANDIT <vishantpandit80@gmail.com>
2026-02-22 21:16:12 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,13 @@
+from tkinter import*
+from tkinter import messagebox              #for message box info popup
+root=Tk()
+root.geometry("300x300")
+root.title("warning")
+root.config(bg="pink")
+def msg():
+    messagebox.showinfo("info","show warning:")
+    
+bt=Button(root,text="press",bd=3,background="pink",command=msg)
+bt.pack(pady=20)
+
+root.mainloop()
